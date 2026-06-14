6731c82784535274fbb2a7238213a0cccd3ddb07
yPerfectBR <kauagabriel571@gmail.com>
2026-06-14 17:52:45 -0300

    feat: added logger test module

---
+++ tests/Orion.Logger.Tests/LoggerConfigTests.cs
@@ -0,0 +1,41 @@
+using Orion.Config;
+using Log = Orion.Logger.Logger;
+
+namespace Orion.Logger.Tests;
+
+public sealed class LoggerConfigTests
+{
+    [Fact]
+    public void LoadServerJson_EnablesConfiguredLevels()
+    {
+        string configPath = Path.GetFullPath(Path.Combine(
+            AppContext.BaseDirectory, "config", "server.json"));
+
+        OrionInfo.Load(configPath);
+        Log.Configure(OrionInfo.Logging.LogLevel);
+
+        Assert.True(Log.IsEnabled(LogCategory.RakNet, LogLevel.Info));
+        Assert.True(Log.IsEnabled(LogCategory.Protocol, LogLevel.Debug));
+    }
+
+    [Fact]
+    public void DisabledLevel_IsFiltered()
+    {
+        var levels = new LogLevelConfig
+        {
+            RakNet = new CategoryLogLevel
+            {
+                Debug = false,
+                Info = true,
+                Warn = true,
+                Err = true,
+                Chat = true
+            }
+        };
+
+        Log.Configure(levels);
+
+        Assert.False(Log.IsEnabled(LogCategory.RakNet, LogLevel.Debug));
+        Assert.True(Log.IsEnabled(LogCategory.RakNet, LogLevel.Info));
+    }
+}
