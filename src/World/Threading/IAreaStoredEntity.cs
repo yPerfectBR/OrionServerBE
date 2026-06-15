a5d6e7d7d4ad7e8afe199aeef3b57e4f30b64f93
yPerfectBR <kauagabriel571@gmail.com>
2026-06-14 22:12:54 -0300

    feat(world): add AreaShard per threading area

---
+++ src/World/Threading/IAreaStoredEntity.cs
@@ -0,0 +1,6 @@
+namespace Orion.World.Threading;
+
+/// <summary>
+/// Marker for entities attached to an area shard (implemented by <see cref="Orion.Entity.Entity"/>).
+/// </summary>
+public interface IAreaStoredEntity;
