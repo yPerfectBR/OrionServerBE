f25d582179d3c739f844602c1243bd0fa33ea0a3
yPerfectBR <kauagabriel571@gmail.com>
2026-06-14 22:12:54 -0300

    test(orion): add item registry tests

---
+++ tests/Orion.Game.Tests/ItemRegistryTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using Orion.Item;
+
+namespace Orion.Game.Tests;
+
+public sealed class ItemRegistryTests
+{
+    [Fact]
+    public void EnsureLoaded_registers_curated_grass_block()
+    {
+        ItemRegistry.EnsureLoaded();
+        ItemType? grass = ItemType.Get("minecraft:grass_block");
+        Assert.NotNull(grass);
+        Assert.True(grass!.NetworkId > 0);
+    }
+}
